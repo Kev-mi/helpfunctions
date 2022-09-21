6c367cb5613ea74f5d49d3a153ff1d7bccd0dce1
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-09-21 19:08:53 +0200

    Update functions.r

--- functions.r
+++ functions.r
@@ -10,4 +10,13 @@ n_choose_k <- function(n, k){
   factorial(n)/(factorial(k)*(factorial(n-k)))
 }
 
+binomial_distribution_sum <- function(n, prob, upper_bound){
+    individual_prob = 0
+    for (x in 0:upper_bound)
+    {individual_prob <- individual_prob + ((1-prob)**(n-x)*(choose(n, x))*(prob)**(x))}
+    return (individual_prob)
+}
+
+print(binomial_distribution_sum(12, 0.4, 6))
+
 print(n_choose_k(7,3))
